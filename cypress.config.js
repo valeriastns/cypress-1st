c48977234c37369d6e6de753c809cefdde3eb5cb
Valeriia Sharybina <valeriastanis@gmail.com>
2024-10-21 16:07:19 +0200

    connected cypress dashboard, h/w 5.2.1

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+   projectId: "czif38",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {  // implement node event listeners here
     },
   },
 });
