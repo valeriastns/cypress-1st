26b3cb6b20149937030e720ea15a3554f03107ce
Valeriia Sharybina <valeriastanis@gmail.com>
2024-10-29 13:43:27 +0100

    added config and script for staging tests,h/w 5.2.3

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require("cypress");
 
-module.exports = {
+module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com',
+    setupNodeEvents(on, config) {
+      if (config.env.ENV === 'staging') {
+        config.baseUrl = 'https://sqlverifier-staging-08050d656f7a.herokuapp.com';
+      }
+      return config;
+    },
   },
-}
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,8 @@
 Cypress.Commands.add('loginAsStudent', () => {
 
     cy.visit('/login');
-    cy.get('[data-cy="username"]').type('user-student1');
-    cy.get('[data-cy="password"]').type('student');
+    cy.get('[data-cy="username"]').type('student1');
+    cy.get('[data-cy="password"]').type('student123');
     cy.get('[data-cy="submit"]').click();
     cy.url().should('include', '/?page=1&sort=id,asc');
-
   });
