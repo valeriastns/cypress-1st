e855eadb1ff00822ac274110a63cc713ec1cbbf1
Valeriia Sharybina <valeriastanis@gmail.com>
2024-10-31 13:04:02 +0100

    added custom commands,h/w 5.3.1

--- cypress/e2e/verifier/headerLinks.cy.js
+++ cypress/e2e/verifier/headerLinks.cy.js
@@ -4,36 +4,36 @@ describe("Header menu links test", () => {
   });
 
   it("Check if account menu link working", () => {
-    cy.get('[data-cy="accountMenu"]').click();
-    cy.get('[data-cy="accountMenu"].dropdown').should("have.class", "show");
+    cy.openMenu("accountMenu");
+    cy.assertMenuVisible("accountMenu");
   });
 
   it("Check if password menu link working", () => {
-    cy.get('[data-cy="accountMenu"]').click();
+    cy.openMenu("accountMenu");
     cy.get('[data-cy="passwordItem"]').click();
     cy.url().should("include", "/account/password");
   });
 
   it("Check if settings menu link working", () => {
-    cy.get('[data-cy="accountMenu"]').click();
+    cy.openMenu("accountMenu");
     cy.get('[data-cy="settings"]').click();
     cy.url().should("include", "/account/settings");
   });
 
   it("Check if sign-out menu link working", () => {
-    cy.get('[data-cy="accountMenu"]').click();
+    cy.openMenu("accountMenu");
     cy.get('[data-cy="logout"]').click();
     cy.url().should("include", "/logout");
   });
 
   it("Check if swagger menu link working", () => {
-    cy.get('[data-cy="docsMenu"]').click();
-    cy.get('[data-cy="docsMenu"]').should("have.class", "show");
+    cy.openMenu("docsMenu");
+    cy.assertMenuVisible("docsMenu");
   });
 
   it("Check if api menu link working", () => {
-    cy.get('[data-cy="docsMenu"]').click();
-    cy.get('[data-cy="docsMenu"]').should("have.class", "show");
+    cy.openMenu("docsMenu");
+    cy.assertMenuVisible("docsMenu");
     cy.get('[data-cy="docsMenu"] > div.dropdown-menu')
       .should("have.class", "show")
       .click();
@@ -41,21 +41,21 @@ describe("Header menu links test", () => {
   });
 
   it("Check if entities menu link working", () => {
-    cy.get('[data-cy="entity"]').click();
-    cy.get('[data-cy="entity"]').should("have.class", "show");
+    cy.openMenu("entity");
+    cy.assertMenuVisible("entity");
   });
 
   it("Check if task menu link is working", () => {
-    cy.get('[data-cy="entity"]').click();
-    cy.get('[data-cy="entity"]').should("have.class", "show");
-    cy.get(".dropdown-item").should("be.visible").first().click();
+    cy.openMenu("entity");
+    cy.assertMenuVisible("entity");
+    cy.clickMenuItem("entity", 0);
     cy.url().should("include", "/task?page=1&sort=id,asc");
   });
 
   it("Check if user-task menu link is working", () => {
-    cy.get('[data-cy="entity"]').click();
-    cy.get('[data-cy="entity"]').should("have.class", "show");
-    cy.get(".dropdown-item").should("be.visible").eq(1).click();
+    cy.openMenu("entity");
+    cy.assertMenuVisible("entity");
+    cy.clickMenuItem("entity", 1);
     cy.url().should("include", "/user-task");
   });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,5 +4,17 @@ Cypress.Commands.add('loginAsStudent', () => {
     cy.get('[data-cy="username"]').type('student1');
     cy.get('[data-cy="password"]').type('student123');
     cy.get('[data-cy="submit"]').click();
-   // cy.url().should('include', '/?page=1&sort=id,asc');
+   ///cy.url().should('include', '/?page=1&sort=id,asc');
+  });
+
+  Cypress.Commands.add("openMenu", (menuDataCy) => {
+    cy.get(`[data-cy="${menuDataCy}"]`).click();
+  });
+  
+  Cypress.Commands.add("assertMenuVisible", (menuDataCy) => {
+    cy.get(`[data-cy="${menuDataCy}"]`).should("have.class", "show");
+  });
+  
+  Cypress.Commands.add("clickMenuItem", (menuDataCy, index) => {
+    cy.get(`[data-cy="${menuDataCy}"] .dropdown-item`).eq(index).click();
   });
